a82340dcc43469baf31597e947f741afdeed2d53
Laura <51169863+lauralyotard@users.noreply.github.com>
2021-04-04 19:24:35 +0200

    all

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ParcoursComponent} from './links_menu/parcours/parcours.component';
 import {HomeComponent} from './home/home.component';
+import {PassionsComponent} from './links_menu/passions/passions.component';
+import {StrengthsComponent} from './links_menu/strengths/strengths.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'parcours', component: ParcoursComponent }
+  { path: 'parcours', component: ParcoursComponent },
+  { path: 'passions', component: PassionsComponent },
+  { path: 'atouts-et-competences', component: StrengthsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import {RouterModule} from '@angular/router';
 import { ParcoursComponent } from './links_menu/parcours/parcours.component';
 import { HomeComponent } from './home/home.component';
+import {A11yModule} from '@angular/cdk/a11y';
+import { PassionsComponent } from './links_menu/passions/passions.component';
+import { StrengthsComponent } from './links_menu/strengths/strengths.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +25,9 @@ import { HomeComponent } from './home/home.component';
     MenuComponent,
     FooterComponent,
     ParcoursComponent,
-    HomeComponent
+    HomeComponent,
+    PassionsComponent,
+    StrengthsComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +37,9 @@ import { HomeComponent } from './home/home.component';
     RouterModule,
     AppRoutingModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatIconModule,
+    A11yModule
   ],
   providers: [],
   bootstrap: [AppComponent]
