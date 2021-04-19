ec437181aeac996f3183109afdb1cb5b4c49b267
Laura <51169863+lauralyotard@users.noreply.github.com>
2021-04-19 16:59:59 +0200

    purge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ParcoursComponent} from './links_menu/parcours/parcours.component';
-import {HomeComponent} from './home/home.component';
-import {ProjetsComponent} from './links_menu/projets/projets.component';
-import {StrengthsComponent} from './links_menu/strengths/strengths.component';
-import {ConfidentialiteComponent} from './links_infos/confidentialite/confidentialite.component';
-import {CguComponent} from './links_infos/cgu/cgu.component';
-import {MaintenanceComponent} from './links_infos/maintenance/maintenance.component';
-import {WhoIAmComponent} from './links_menu/who-i-am/who-i-am.component';
-import {BigfiitComponent} from './links_menu/bigfiit/bigfiit.component';
-import {ExperiencesComponent} from './links_menu/experiences/experiences.component';
-import {ContactComponent} from './links_infos/contact/contact.component';
-import {CvComponent} from './cv/cv.component';
-import {UnivhairComponent} from './links_menu/univhair/univhair.component';
-import {ProgestComponent} from './links_menu/progest/progest.component';
+
+import { HomeComponent } from './home/home.component';
+import { ProjetsComponent } from './links_menu/projets/projets.component';
+import { ConfidentialiteComponent } from './links_infos/confidentialite/confidentialite.component';
+import { CguComponent } from './links_infos/cgu/cgu.component';
+import { MaintenanceComponent } from './links_infos/maintenance/maintenance.component';
+import { BigfiitComponent } from './links_menu/bigfiit/bigfiit.component';
+import { ExperiencesComponent } from './links_menu/experiences/experiences.component';
+import { ContactComponent } from './links_infos/contact/contact.component';
+import { CvComponent } from './cv/cv.component';
+import { UnivhairComponent } from './links_menu/univhair/univhair.component';
+import { ProgestComponent } from './links_menu/progest/progest.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'parcours', component: ParcoursComponent },
   { path: 'projets', component: ProjetsComponent },
   { path: 'univhair', component: UnivhairComponent },
   { path: 'progest', component: ProgestComponent },
   { path: 'experiences', component: ExperiencesComponent },
   { path: 'cv', component: CvComponent },
-  { path: 'atouts-et-competences', component: StrengthsComponent },
   { path: 'politique-de-confidentalite', component: ConfidentialiteComponent },
   { path: 'conditions-generales-d-utilisation', component: CguComponent },
   { path: 'mises-a-jour', component: MaintenanceComponent },
-  { path: 'who-i-am', component: WhoIAmComponent },
   { path: 'bigfiit', component: BigfiitComponent },
   { path: 'contact', component: ContactComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
+import {A11yModule} from '@angular/cdk/a11y';
+import {RouterModule} from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,19 +12,12 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
-import {RouterModule} from '@angular/router';
-import { ParcoursComponent } from './links_menu/parcours/parcours.component';
 import { HomeComponent } from './home/home.component';
-import {A11yModule} from '@angular/cdk/a11y';
 import { ProjetsComponent } from './links_menu/projets/projets.component';
-import { StrengthsComponent } from './links_menu/strengths/strengths.component';
 import { ConfidentialiteComponent } from './links_infos/confidentialite/confidentialite.component';
 import { CguComponent } from './links_infos/cgu/cgu.component';
 import { MaintenanceComponent } from './links_infos/maintenance/maintenance.component';
-import { ScrollTopComponent } from './scroll-top/scroll-top.component';
-import { WhoIAmComponent } from './links_menu/who-i-am/who-i-am.component';
 import { BigfiitComponent } from './links_menu/bigfiit/bigfiit.component';
 import { ExperiencesComponent } from './links_menu/experiences/experiences.component';
 import { ContactComponent } from './links_infos/contact/contact.component';
@@ -33,17 +28,12 @@ import { ProgestComponent } from './links_menu/progest/progest.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
     FooterComponent,
-    ParcoursComponent,
     HomeComponent,
     ProjetsComponent,
-    StrengthsComponent,
     ConfidentialiteComponent,
     CguComponent,
     MaintenanceComponent,
-    ScrollTopComponent,
-    WhoIAmComponent,
     BigfiitComponent,
     ExperiencesComponent,
     ContactComponent,
