3c77fdeb86dfbd1df17cf1d2645799463ae75170
Laura <laura.direct@yahoo.com>
2021-04-23 14:07:53 +0200

    Update cgu.component.ts

--- src/app/links_infos/cgu/cgu.component.ts
+++ src/app/links_infos/cgu/cgu.component.ts
@@ -10,8 +10,6 @@ export class CguComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    console.log('Margo always loved mysteries. And in everything that came afterward, I could never stop thinking that maybe she loved mysteries so much that she became one.');
-    console.log('La face cachée de Margo');
   }
 
 }
